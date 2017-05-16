223d938a5a3bd6d12cdc25cf886e591d60ad09ea
Nynia <jokeforce@gmail.com>
2017-05-16 17:12:10 +0800

    debug

--- app/tasks/datatask.py
+++ app/tasks/datatask.py
@@ -51,4 +51,4 @@ def datahandle(self, filenames, type, remark, create_person):
     print success_count, fail_count, illegal_numbers
 
     return {'success': success_count, 'fail': fail_count, 'repeat': repeat_count, 'status': 'Task completed!',
-                    'result': illegal_numbers}
+                    'result': 42}
