32d179b6e8e6c4fec4294d7e667a365584c7bfdb
Nynia <jokeforce@gmail.com>
2017-05-17 20:12:54 +0800

    debug

--- app/main/views.py
+++ app/main/views.py
@@ -196,9 +196,7 @@ def download():
     filename = request.args.get('filename')
     print filename,repr(filename)
     print type(filename)
-
-    print repr(filename.encode('utf-8'))
-    fp = open('./res/' + filename.encode('gbk'), 'r')
+    fp = open('../res/' + filename, 'r')
     content = ''
     for line in fp.readlines():
         content += line.strip('\n') + '\r\n'
