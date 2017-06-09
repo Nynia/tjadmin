2ed579f748fc8d3eccbb4baafde57c7751713e07
Nynia <jokeforce@gmail.com>
2017-06-09 10:59:31 +0800

    debug

--- tests/redis_test.py
+++ tests/redis_test.py
@@ -2,8 +2,5 @@ import datetime
 import redis
 redisClient = redis.StrictRedis(host='localhost',port=6379,db=2)
 
-for i in range(1000,9999):
-    redisClient.hset(str(i), "id", i)
-    redisClient.hset(str(i), "timestamp", datetime.datetime.now().strftime('%Y%m%d%H%M%S'))
 
 print redisClient.hget('2999','id'),redisClient.hget('2999','timestamp')
