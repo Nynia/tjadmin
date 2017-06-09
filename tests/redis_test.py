afce99b3bf7dc0bc8686fd371d93b89a96217ae9
Nynia <jokeforce@gmail.com>
2017-06-09 14:44:24 +0800

    debug

--- tests/redis_test.py
+++ tests/redis_test.py
@@ -5,3 +5,6 @@ redisClient = redis.StrictRedis(host='localhost',port=6379,db=2)
 
 print redisClient.hget('2999','id'),redisClient.hget('2999','timestamp')
 print redisClient.dbsize(),redisClient.hlen('2999')
+print redisClient.hexists('100','id')
+print redisClient.hexists('1000','ids')
+print redisClient.hget('100','id')
